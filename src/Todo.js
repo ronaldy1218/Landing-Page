12cfa54c31975c9159afd9448d4750411f47c572
ronaldy1218 <ronaldy1218@yahoo.com>
2022-11-21 00:34:56 -0700

    react

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-export default function Todo({ todo }) {
-  return (
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
+
+    return (
     <div>
         <label>
-            <input type="checkbox" checked={todo.complete}></input>
+            <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}></input>
             {todo.name}
         </label>
     </div>
